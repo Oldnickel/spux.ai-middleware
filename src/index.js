f754cea63c5aee699f4f4c1c5d3e09b6469950e8
cnyamao <collins@aiceafrica.com>
2023-05-23 02:03:05 +0300

    added certs

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
 
+const https = require('https');
+const fs = require('fs');
+
+console.log(__dirname);
+
+const privateKey = fs.readFileSync(__dirname + '/../certs/privkey.pem', 'utf8');
+const certificate = fs.readFileSync(__dirname + '/../certs/fullchain.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
+const httpsServer = https.createServer(credentials, app);
+
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
-  server = app.listen(config.port, () => {
+  server = httpsServer.listen(config.port, () => {
     logger.info(`Listening to port ${config.port}`);
   });
 });
