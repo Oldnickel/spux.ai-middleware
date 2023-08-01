4b8b0510a05d37c1971126241a0e344845383e5a
cnyamao <collins@aiceafrica.com>
2023-08-01 16:23:49 +0300

    tried new config type

--- render/plugin.js
+++ render/plugin.js
@@ -141,7 +141,7 @@
         document.head.appendChild(sweetAlertScript);
 
         // Load the Chatwoot SDK
-        const chatwootScript = document.createElement('script');
+        /* const chatwootScript = document.createElement('script');
         chatwootScript.src = 'https://app.chatwoot.com/packs/js/sdk.js';
         chatwootScript.defer = true;
         chatwootScript.async = true;
@@ -152,8 +152,23 @@
                 websiteToken: 'urfwPZD2euXcHgpWA5vaDycS', // Replace with your Chatwoot website token
                 baseUrl: 'https://app.chatwoot.com',
             });
-        };
+        }; */
     }
+
+    (function (d, t) {
+        var BASE_URL = "https://app.chatwoot.com";
+        var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+        g.src = BASE_URL + "/packs/js/sdk.js";
+        g.defer = true;
+        g.async = true;
+        s.parentNode.insertBefore(g, s);
+        g.onload = function () {
+            window.chatwootSDK.run({
+                websiteToken: 'urfwPZD2euXcHgpWA5vaDycS',
+                baseUrl: BASE_URL
+            })
+        }
+    })(document, "script");
 
     // Function to initialize the plugin
     function init() {
