4b7503738855a529e1bb9e8c80a7e13fc968d420
cnyamao <collins@aiceafrica.com>
2023-08-04 14:45:13 +0300

    adjusted card message

--- src/services/chat.service.js
+++ src/services/chat.service.js
@@ -12,28 +12,29 @@ const findProducts = async (productName) => {
     //console.log('products: ', products);
 
     if (products) {
+        const productInfo = products[0]
         const richCardMessage = {
             fulfillmentMessages: [
                 {
                     card: {
-                        title: `Product Information - ${productName}`,
-                        subtitle: productInfo.description,
-                        imageUri: productInfo.imageURL,
+                        title: `Product Information - ${productInfo.name}`,
+                        subtitle: productInfo.brand,
+                        imageUri: 'https://salesmate.aiceafrica.com/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F8144%2F8724%2Ffiles%2Fmockup-de719380.webp%3Fv%3D1682956169&w=2048&q=75',
                         buttons: [
                             {
                                 text: "Buy Now",
-                                postback: `https://example.com/buy-product/${productInfo.id}`,
+                                postback: `https://example.com/buy-product/${productInfo.name}`,
                             },
                             {
                                 text: "More Details",
-                                postback: `https://example.com/product-details/${productInfo.id}`,
+                                postback: `https://example.com/product-details/${productInfo.name}`,
                             },
                         ],
                     },
                 },
             ],
         };
-        return res.json(fulfillmentMessages);
+        return res.json(richCardMessage);
     } else {
         return 'No products matching your query have been found.';
     }
